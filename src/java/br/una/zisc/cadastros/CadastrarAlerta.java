b623e305c63a88e31ec45aab7fe7e9b09c2c4944
mikef <mikef@192.168.1.3>
2016-11-02 11:33:46 -0200

    Classe CadastrarAlerta retornando json

--- src/java/br/una/zisc/cadastros/CadastrarAlerta.java
+++ src/java/br/una/zisc/cadastros/CadastrarAlerta.java
@@ -7,12 +7,14 @@ package br.una.zisc.cadastros;
 
 import br.una.zisc.consultas.Consultas;
 import br.una.zisc.dao.Alerta;
+import com.google.gson.Gson;
 import java.util.Date;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -23,21 +25,30 @@ import javax.ws.rs.core.MediaType;
 public class CadastrarAlerta {
 
     @POST
+    @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public void alerta(@FormParam("email") String email,
-                       @FormParam("logHora") Date logHora,
-                       @FormParam("longitude") String longitude,
-                       @FormParam("latitude") String latitude,
-                       @FormParam("bairro") String bairro,
-                       @FormParam("cidade") String cidade,
-                       @FormParam("estado") String estado,
-                       @FormParam("obs") String obs,
-                       @FormParam("tipo") String tipo){
-        
-        Consultas con = new Consultas();        
-        Alerta alerta = new Alerta(con.buscaUsuario(email), logHora, longitude, latitude, bairro, cidade, estado, obs, tipo, true);
-        Cadastros cad = new Cadastros();
-        cad.cadastraralerta(alerta);
+    public String alerta(@FormParam("email") String email,
+            @FormParam("logHora") Date logHora,
+            @FormParam("longitude") String longitude,
+            @FormParam("latitude") String latitude,
+            @FormParam("bairro") String bairro,
+            @FormParam("cidade") String cidade,
+            @FormParam("estado") String estado,
+            @FormParam("obs") String obs,
+            @FormParam("tipo") String tipo) {
 
+        try {
+            Consultas con = new Consultas();
+            Alerta alerta = new Alerta(con.buscaUsuario(email), logHora, longitude, latitude, bairro, cidade, estado, obs, tipo, true);
+            Cadastros cad = new Cadastros();
+            cad.cadastraralerta(alerta);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            Gson gson = new Gson();
+            String json = gson.toJson("NAO");
+            return json;
+        }
+        Gson gson = new Gson();
+        String json = gson.toJson("OK");
+        return json;
     }
 }
